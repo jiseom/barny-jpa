9e51fb14f2cc4359aeff925a69c29a90e2016a07
jiseon <seom228@gmail.com>
2022-04-08 14:49:35 +0900

    어카운트 도메인 추가

---
+++ src/main/java/edu/bit/ex/domain/Account.java
@@ -0,0 +1,56 @@
+package edu.bit.ex.domain;
+
+import javax.persistence.Entity;
+
+import edu.bit.ex.domain.constant.Role;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue
+    public Long id;
+
+    @Column(nullable = false)
+    private String accountId;
+
+    @Column(nullable = false)
+    public String name;
+
+    @Column(nullable = false)
+    public String nickname;
+
+    @Column(unique = true, nullable = false)
+    public String email;
+
+    @Size(min = 8, max = 100)
+    public String password;
+
+    public String payment;
+
+    @Column(nullable = false)
+    public String tel;
+
+    @Column(nullable = false)
+    public String address;
+
+    @Column(nullable = false)
+    public LocalDate dateOfBirth;
+
+    private int point;
+
+    private int subscribe;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
