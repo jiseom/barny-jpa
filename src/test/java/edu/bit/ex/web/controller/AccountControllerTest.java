0c423cdffb36b37dc24ed780743a871826b6d498
jiseon <seom228@gmail.com>
2022-04-15 23:15:17 +0900

    AccountController 테스트 수정

--- src/test/java/edu/bit/ex/web/controller/AccountControllerTest.java
+++ src/test/java/edu/bit/ex/web/controller/AccountControllerTest.java
@@ -8,8 +8,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.sql.DataSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
@@ -28,6 +34,12 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    DataSource dataSource;
 
     @BeforeEach
     void deleteAll() {
