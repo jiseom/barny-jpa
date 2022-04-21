650a08e488579af5bcbde1d45ca3f823c2539fd1
jiseon <seom228@gmail.com>
2022-04-22 03:55:44 +0900

    마이바티스 컨트롤러 리팩토링

--- src/main/java/edu/bit/ex/controller/OrderControllerDeprecated.java
+++ src/main/java/edu/bit/ex/controller/OrderControllerDeprecated.java
@@ -24,7 +24,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-public class OrderController {
+public class OrderControllerDeprecated {
 	
 	@Autowired
 	private CartServiceDeprecated cartService;
