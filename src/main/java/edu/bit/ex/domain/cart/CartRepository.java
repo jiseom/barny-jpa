7ceba33f66b218f91a0da280be80f2f3b7afd21e
jiseon <seom228@gmail.com>
2022-04-22 01:56:04 +0900

    CartRepository - 장바구니 선택 삭제 쿼리 추가

--- src/main/java/edu/bit/ex/domain/cart/CartRepository.java
+++ src/main/java/edu/bit/ex/domain/cart/CartRepository.java
@@ -14,7 +14,7 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     @Query("SELECT c " +
             "FROM Cart c " +
             "LEFT JOIN c.cartCustomer a " +
-            " ON a.id =:id ")
+            "WHERE c.cartCustomer.id =:id ")
     List<Cart> getAccountCart(@Param("id") Long id);
 
 
@@ -22,48 +22,18 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
      * 장바구니 내역
      */
     @Query("SELECT c " +
-            " FROM  Cart c ,Product p" +
-            "LEFT JOIN c.selectedItem p WHERE c.id =:cartCustomerId " +
-            "AND p.id in(:selectedItemId)")
-    List<Cart> getCartList(@Param("id") Long cartCustomerId, List<Product>selectedItemId);
+            " FROM  Cart c ,Account a " +
+            " JOIN c.selectedItem p WHERE c.cartCustomer.id =:cartCustomerId " +
+            "AND c.selectedItem.id in(:selectedItemId)")
+    List<Cart> getCartList(@Param("id") Long cartCustomerId, List<Product> selectedItemId);
 
-//
-//    /**
-//     * 상품으로 장바구니 찾기
-//     */
-//    @Query("SELECT c " +
-//            "FROM Cart c ,Product p , Account a LEFT JOIN c.selectedItem WHERE a.id =:cartCustomerId AND p.id =:selectedItemId")
-//    Cart findBySelectedItem(@Param("id")Long cartCustomerId,@Param("id") Long selectedItemId);
+    @Query("SELECT c " +
+            " FROM  Cart c  " +
+            "LEFT JOIN c.selectedItem p WHERE c.cartCustomer.id =:cartCustomerId " +
+            "AND c.selectedItem.id =:id")
+    Cart getCart( Long cartCustomerId, @Param("id") Long id);
 
 
-    //      <!-- 장바구니 총 합계 -->
-//    <select id="sumMoney" resultType="int">
-//	<![CDATA[
-//    select nvl(sum(price*product_qty), 0) from cart c, product p
-//    where c.product_id = p.product_id and member_idx = #{member_idx}
-//	]]>
-//	</select>
-//}
-//
-
-
-
-
-
-
-//<!-- 장바구니 목록 출력 -->
-//    <select id="cartList" resultType="edu.bit.ex.vo.cart.CartVO">
-//        <![CDATA[
-//    SELECT I.IMAGE_ROUTE, M.MEMBER_IDX, P.PRODUCT_ID, P.PRODUCT_NAME, P.PRICE, C.PRODUCT_QTY
-//    FROM MEMBER M, PRODUCT P, CART C, IMAGE I
-//    WHERE M.MEMBER_IDX = C.MEMBER_IDX AND P.PRODUCT_ID = C.PRODUCT_ID AND I.PRODUCT_ID = P.PRODUCT_ID AND M.MEMBER_IDX = #{MEMBER_ID}
-//    ORDER BY MEMBER_IDX ASC
-//        ]]>
-//    </select>
-
-
-
-
-
+    void deleteAllByIdIn(List<Long> ids);
 
 }
