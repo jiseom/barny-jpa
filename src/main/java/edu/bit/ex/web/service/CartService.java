5b1eaaba07b3ff8fb952e96b99bc7e74a0831b91
jiseon <seom228@gmail.com>
2022-04-22 01:52:00 +0900

    CartService - 장바구니 내역 기능 추가

--- src/main/java/edu/bit/ex/web/service/CartService.java
+++ src/main/java/edu/bit/ex/web/service/CartService.java
@@ -6,14 +6,15 @@ import edu.bit.ex.domain.cart.CartRepository;
 import edu.bit.ex.domain.product.Product;
 import edu.bit.ex.domain.product.ProductRepository;
 import edu.bit.ex.web.dto.CreateCartForm;
+import edu.bit.ex.web.dto.DeleteCartForm;
 import edu.bit.ex.web.dto.UpdateCartForm;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Transactional
 @RequiredArgsConstructor
@@ -24,6 +25,9 @@ public class CartService {
     private final CartRepository cartRepository;
     private final ModelMapper modelMapper;
 
+    /**
+     * 장바구니 담기
+     */
     public Cart addCart(Product product, Account account, CreateCartForm createCartForm) {
         createCartForm.setCartCustomer(account);
         createCartForm.setSelectedItem(product);
@@ -36,33 +40,19 @@ public class CartService {
     /**
      * 나의 장바구니
      */
-    @Transactional(readOnly = true)
+//    @Transactional(readOnly = true)
     public List<Cart> getCartList(Account account) {
-        Long CustomerId = account.getId();
         //회원의 카트 가져오기
-        //   List<Cart> accountCart = cartRepository.getAccountCart(CustomerId);
-        return cartRepository.getAccountCart(CustomerId);
+        List<Cart> cartList = new ArrayList<>();
+        //1.현재 로그인한 회원의 장바구니 엔티티를 조회
+        List<Cart> accountCart = cartRepository.getAccountCart(account.getId());
+        //2. 장바구니에 상품을 한 번도 안담았을 경우 장바구니 엔티티가 없으므로 빈 리스트를 반환
+        if (accountCart == null) {
+            return cartList;
+        }
+        //3. 장바구니에 담겨 있는 상품 정보를 조회
+        return cartRepository.getAccountCart(account.getId());
     }
 
-
-//    public int sumMoney(Account account) {
-//        //회원 카트 가져오기
-//        Cart accountCart = cartRepository.getAccountCart(account.getId());
-//        //해당 회원의 상품 번호로 카트 가져오기
-//        List<Cart> findCart = cartRepository.findBySelectedItem(accountCart.getSelectedItem().getId());
-//        //카트의 가격과 수량 가져오기
-//        int price = 0;
-//        int quantity = 0;
-//        for (Cart cart : findCart) {
-//            price = cart.getSelectedItem().getPrice();
-//            quantity = cart.getQuantity();
-//
-//        }
-//        return price * quantity;
-////        int price =  findCart.getSelectedItem().getPrice();
-////        int quantity = findCart.getQuantity();
-//
-////        return price * quantity;
-//
-//    }
 }
+
